1e083033810e034a8131ca5e39095c97c660dc59
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-02-16 09:43:07 +0700

    Add files via upload

---
+++ tesStreamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title("Hello, Streamlit!")
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("Slider:", slider_val, "Checkbox:", checkbox_val)
