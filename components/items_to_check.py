1b9e0f9c35a292c9bfa2653753cebc659e86c1c3
Fatih Zahrani <fatihzahrani2005@gmail.com>
2026-03-24 08:49:00 +0700

    Update items_to_check.py

--- components/items_to_check.py
+++ components/items_to_check.py
@@ -127,111 +127,110 @@ SECTIONS = {
     ]
 }
 
-if st.checkbox("Show Items Check"):
-    def render_items_to_check(df, item_check):
+def render_items_to_check(df, item_check):
+
+    st.markdown("## ITEMS TO CHECK")
     
-        st.markdown("## ITEMS TO CHECK")
+    st.write ("NOTE: All documents/package from Design/Customer must be updated for every stage of the build including Mass Production")
+    if not item_check:
+        st.warning("No items parsed from PDF")
+
+    for item in item_check:
+        st.write(item)
         
-        st.write ("NOTE: All documents/package from Design/Customer must be updated for every stage of the build including Mass Production")
-        if not item_check:
-            st.warning("No items parsed from PDF")
-    
-        for item in item_check:
-            st.write(item)
-            
-        engineer_list = [""] + df["ER"].unique().tolist()
-        st.markdown(
-            """
-            <style>
-            
-            div[data-testid="stExpander"] {
-                background-color:#ffffff;
-                border-radius:8px;
-                border:1px solid #d0d7de;
-            }
-            
-            div[data-testid="stExpander"] summary {
-                font-weight:600;
-                font-size:14px;
-            }
-            
-            </style>
-            """, unsafe_allow_html=True)
-        with st.container(border=True, height=600):  
-    
-            col1,col2,col3,col4, col5 = st.columns([3,2,2,2,4])
-    
-            col1.markdown("**Check Item**")
-            col2.markdown("**")
-            col3.markdown("**PIC**")
-            col4.markdown("**Target Finish**")
-            col5.markdown("**Remarks**")
+    engineer_list = [""] + df["ER"].unique().tolist()
+    st.markdown(
+        """
+        <style>
         
-            st.markdown("---")
+        div[data-testid="stExpander"] {
+            background-color:#ffffff;
+            border-radius:8px;
+            border:1px solid #d0d7de;
+        }
         
-            for section, items in SECTIONS.items():
-               with st.expander(section, expanded=False):
-                   for item in items:
-                       if item == "ICT Program / Fixture":
-                            c1,c2,c3,c4,c5 = st.columns([3,2,2,2,4])
-                        
-                            with c1:
-                                st.write("ICT Program / Fixture")
-                                
-                            with c3:
-                                st.multiselect("", engineer_list, key="pic_ict", label_visibility="collapsed")
-                                
-                            with c4:
-                                st.text_area("", key="target_ict", label_visibility="collapsed")
-                                
-                            with c5:
-                                st.text_area("", key="remark_ict", label_visibility="collapsed")
-                        
-                            for left, right in ICT_ROWS:
-                                c1,c2,c3,c4,c5 = st.columns([3,2,2,2,4])
-                        
-                                with c1:
-                                    st.checkbox(left, key=f"ict_{normalize_key(left)}")
-                        
-                                with c2:
-                                    if right:
-                                        st.checkbox(right, key=f"ict_{normalize_key(right)}")
-                        
-                                with c3:
-                                    st.multiselect(
-                                        "",
-                                        engineer_list,
-                                        key=f"pic_ict_{normalize_key(left)}",
-                                        label_visibility="collapsed"
-                                    )
-                        
-                                with c4:
-                                    key_target = f"target_ict_{normalize_key(left)}"
-                                    value_target = st.session_state.get(key_target, "")
+        div[data-testid="stExpander"] summary {
+            font-weight:600;
+            font-size:14px;
+        }
+        
+        </style>
+        """, unsafe_allow_html=True)
+    with st.container(border=True, height=600):  
+
+        col1,col2,col3,col4, col5 = st.columns([3,2,2,2,4])
+
+        col1.markdown("**Check Item**")
+        col2.markdown("**")
+        col3.markdown("**PIC**")
+        col4.markdown("**Target Finish**")
+        col5.markdown("**Remarks**")
     
-                                    st.text_area(
-                                        "",
-                                        key=key_target,
-                                        height=100 if len(value_target) > 50 else 60,
-                                        label_visibility="collapsed"
-                                    )
-                        
-                                with c5:
-                                    key_remark = f"remark_ict_{normalize_key(left)}"
-                                    value_remark = st.session_state.get(key_remark, "")
-                                    
-                                    st.text_area(
-                                        "",
-                                        key=key_remark,
-                                        height=120 if len(value_remark) > 50 else 80,
-                                        label_visibility="collapsed"
-                                    )
-                               
-                       else:
-                           render_row(item, engineer_list, section)
-                           
-                          
+        st.markdown("---")
+    
+        for section, items in SECTIONS.items():
+           with st.expander(section, expanded=False):
+               for item in items:
+                   if item == "ICT Program / Fixture":
+                        c1,c2,c3,c4,c5 = st.columns([3,2,2,2,4])
+                    
+                        with c1:
+                            st.write("ICT Program / Fixture")
                             
-    
+                        with c3:
+                            st.multiselect("", engineer_list, key="pic_ict", label_visibility="collapsed")
+                            
+                        with c4:
+                            st.text_area("", key="target_ict", label_visibility="collapsed")
+                            
+                        with c5:
+                            st.text_area("", key="remark_ict", label_visibility="collapsed")
+                    
+                        for left, right in ICT_ROWS:
+                            c1,c2,c3,c4,c5 = st.columns([3,2,2,2,4])
+                    
+                            with c1:
+                                st.checkbox(left, key=f"ict_{normalize_key(left)}")
+                    
+                            with c2:
+                                if right:
+                                    st.checkbox(right, key=f"ict_{normalize_key(right)}")
+                    
+                            with c3:
+                                st.multiselect(
+                                    "",
+                                    engineer_list,
+                                    key=f"pic_ict_{normalize_key(left)}",
+                                    label_visibility="collapsed"
+                                )
+                    
+                            with c4:
+                                key_target = f"target_ict_{normalize_key(left)}"
+                                value_target = st.session_state.get(key_target, "")
+
+                                st.text_area(
+                                    "",
+                                    key=key_target,
+                                    height=100 if len(value_target) > 50 else 60,
+                                    label_visibility="collapsed"
+                                )
+                    
+                            with c5:
+                                key_remark = f"remark_ict_{normalize_key(left)}"
+                                value_remark = st.session_state.get(key_remark, "")
+                                
+                                st.text_area(
+                                    "",
+                                    key=key_remark,
+                                    height=120 if len(value_remark) > 50 else 80,
+                                    label_visibility="collapsed"
+                                )
                            
+                   else:
+                       render_row(item, engineer_list, section)
+                       
+                      
+                        
+
+                       
                     
